cfad9c4057d2435b3c911ca57283ab15d4e38817
User <User@DESKTOP-U19LD9L>
2022-01-25 23:55:15 -0800

    Add project files.

---
+++ OnlineCompiler/Server/Controllers/ExecutionController.cs
@@ -0,0 +1,54 @@
+﻿using OnlineCompiler.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineCompiler.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExecutionController : ControllerBase
+    {
+        private static Dictionary<string, CodeExecutor> _codeExecutors = new Dictionary<string, CodeExecutor>();
+
+        private readonly ILogger<ExecutionController> _logger;
+
+        public ExecutionController(ILogger<ExecutionController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Request for compiling and executing code
+        /// </summary>
+        /// <param name="code">C# code</param>
+        /// <returns>Unique id of the operation</returns>
+        [HttpPost]
+        public string Post([FromBody]string? code)
+        {
+            if (code == null)
+                return "0";
+            string guid = Guid.NewGuid().ToString();
+            CodeExecutor ce = new CodeExecutor(code);
+            _codeExecutors.Add(guid, ce);
+            Task.Run(async () =>
+            {
+                await ce.PerformCodeExecutionAsync();
+                await Task.Delay(5000);
+                _codeExecutors.Remove(guid);
+            });
+            return guid;
+        }
+
+        /// <summary>
+        /// Getting execution information by id
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns>Execution Information</returns>
+        [HttpGet]
+        public ExecutionInfo? Get(string id)
+        {
+            if (!_codeExecutors.ContainsKey(id))
+                return null;
+            return _codeExecutors[id].ExecutionInfo;
+        }
+    }
+}
---
+++ OnlineCompiler/Shared/ExecutionInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCompiler.Shared
+{
+    public class ExecutionInfo
+    {
+        public enum ExecutionStatus
+        {
+            Preparing,
+            Compiling,
+            CompilationError,
+            Running,
+            Finished
+        }
+
+        public ExecutionStatus Status { get; set; }
+        public string Output { get; set; }
+
+        /// <summary>
+        /// Конструктор для десериализации
+        /// </summary>
+        [System.Text.Json.Serialization.JsonConstructor]
+        public ExecutionInfo(){ this.Output = String.Empty; }
+        public ExecutionInfo(ExecutionStatus resultStatus, string Output = "")
+        {
+            this.Status = resultStatus;
+            this.Output = Output;
+        }
+    }
+}
